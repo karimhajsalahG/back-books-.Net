eaeebe25a9a9f55d4116699de1de8635c9728ba9
karimhajsalahG <karimhajsalah056@gmail.com>
2021-10-03 19:22:25 +0200

    first commit

---
+++ Models/BooksDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ApiBooksCrud.Models
+{
+    public partial class BooksDBContext : DbContext
+    {
+    
+
+        public BooksDBContext(DbContextOptions<BooksDBContext> options)
+            : base(options)
+        {
+        }
+
+       
+
+        public virtual DbSet<Book> Books { get; set; }
+    }
+}
